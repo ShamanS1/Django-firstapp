c761fd731e8844d0103eb5a8e13ceac38c74a671
ShamanS1 <shamanhussain@gmail.com>
2023-08-05 22:35:38 +0530

    commit

---
+++ MyFirstApp/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html',)
+
+def add(request):
+    val1 = int(request.POST['number1'])
+    val2 = int(request.POST['number2'])
+    res1 = val1 + val2
+    return render(request, 'home.html',{'result1':res1})
+
+def sub(request):
+    val3 = int(request.GET['number3'])
+    val4 = int(request.GET['number4'])
+    res2 = val3 - val4
+    return render(request, 'home.html',{'result2':res2})
