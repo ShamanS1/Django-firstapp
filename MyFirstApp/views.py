2166569567e99194e5e734d67e9d6765de7115ae
ShamanS1 <shamanhussain@gmail.com>
2023-08-05 23:57:42 +0530

    commit

--- MyFirstApp/views.py
+++ MyFirstApp/views.py
@@ -9,10 +9,10 @@ def add(request):
     val1 = int(request.POST['number1'])
     val2 = int(request.POST['number2'])
     res1 = val1 + val2
-    return render(request, 'home.html',{'result1':res1})
+    return render(request, 'index.html',{'result1':res1})
 
 def sub(request):
     val3 = int(request.GET['number3'])
     val4 = int(request.GET['number4'])
     res2 = val3 - val4
-    return render(request, 'home.html',{'result2':res2})
+    return render(request, 'index.html',{'result2':res2})
